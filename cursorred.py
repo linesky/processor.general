6f74b6242f06f075547cb16999a1f86f02e5b0e8
sky line <32512284+linesky@users.noreply.github.com>
2024-07-05 16:50:19 +0100

    Add files via upload

--- cursorred.py
+++ cursorred.py
@@ -15,13 +15,13 @@ def find_blue_cursor_position(frame):
             
         for xx in range(640):
             
-            blue_channel,green_channel,red_channel=frame[xx,yy]
+            blue_channel,green_channel,red_channel=frame[yy,xx]
             if blue_channel<64 and green_channel<64 and red_channel>128:
                 
                 return (xx, yy)
             
-            else:
-                return None
+
+    return None
             
 
 def main():
